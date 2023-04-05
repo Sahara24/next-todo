79d99557ecde6dcba0f4a81aeac28cc03b192688
Sai Harshith <68738948+Sahara24@users.noreply.github.com>
2023-04-05 19:49:00 +0000

    Added input and buttons for todo

--- my-app/component/UserDashboard.tsx
+++ my-app/component/UserDashboard.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import { MenuContext } from './MenuContext'
+import React, { useState } from "react";
+import { MenuContext } from "./MenuContext";
 
 export default function UserDashboard() {
+  const [isTodo, setIsTodo] = useState(false);
+  const [todo, setTodo] = useState<string | undefined>();
+  const [todoList, setTodoList] = useState<string[]>([])
+
+  const setTodoFun = () => {
+    setTodoList((prev)=>[...prev,todo!])
+    setTodo("")
+  }
+
   return (
     <>
-    <div className='dashboard'>UserDashboard</div>
+      <div className="dashboard">
+        {!isTodo && (
+          <button className="todoBtn" onClick={() => setIsTodo(true)}>
+            ADD TODO
+          </button>
+        )}
+        {isTodo && (
+          <>
+          <div className="todo-container">
+            <input
+              placeholder="Add todo item"
+              value={todo}
+              onChange={(e) => setTodo(e.target.value)}
+            />
+            <button className="addTodoBtn" onClick={()=>setTodoFun()}>ADD</button>
+          </div>
+          <div className="todo-list">
+            {todoList.map((item,index)=>{
+              return(
+                <ol key={index}>
+                  <li>{item}</li>
+                </ol>
+              )
+            })}
+          </div>
+          </>
+        )}
+      </div>
     </>
-  )
+  );
 }
